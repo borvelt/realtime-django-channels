21e9f107011d0af6d49132fbba3515d5a59f5935
borvelt <borvelt@borvelts-MacBook-Pro.local>
2017-01-24 00:23:04 +0330

    more stable

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,50 +1,48 @@
-import json
-
 from channels import Channel
-from channels.auth import channel_session_user, channel_session_user_from_http
+from channels.generic.websockets import (JsonWebsocketConsumer)
 from django.contrib.auth.models import User
-
+import json
 from .models import (Room)
 
 
-@channel_session_user_from_http
-def ws_connect(message, channel):
-    reply = {"accept": False}
-    room = None
-    try:
-        User.objects.filter(username=channel).get()
-    except:
-        message.reply_channel.send(reply)
-        return
-    try:
-        room = Room.find_by_hash_of(channel, message.user)
-        reply = {"accept": True}
-    except:
+class Consumer(JsonWebsocketConsumer):
+    channel_session_user = True
+    http_user = True
+
+    def connect(self, content, **kwargs):
+        reply = {"accept": False}
+        room = None
         try:
-            room = Room(members=[channel, message.user.username])
-            room.save()
+            User.objects.filter(username=kwargs['channel']).get()
+        except:
+            self.message.reply_channel.send(reply)
+            return
+        try:
+            room = Room.find_by_hash_of(kwargs['channel'], self.message.user)
             reply = {"accept": True}
         except:
+            try:
+                room = Room(members=[kwargs['channel'], self.message.user.username])
+                room.save()
+                reply = {"accept": True}
+            except:
+                pass
+        try:
+            assert room
+            self.message.reply_channel.send(reply)
+            room.channel.add(self.message.reply_channel)
+        except:
+            self.message.reply_channel.send(reply)
+
+    def receive(self, content, **kwargs):
+        payload = json.loads(self.message['text'])
+        payload['channel'] = kwargs['channel']
+        payload['reply_channel'] = self.message.content['reply_channel']
+        Channel("chat.receive").send(payload)
+
+    def disconnect(self, message, **kwargs):
+        try:
+            room = Room.find_by_hash_of(self.message.user, kwargs['channel'])
+            room.channel.discard(self.message.reply_channel)
+        except:
             pass
-    try:
-        assert room
-        message.reply_channel.send(reply)
-        room.channel.add(message.reply_channel)
-    except:
-        message.reply_channel.send(reply)
-
-
-def ws_receive(message, channel):
-    payload = json.loads(message['text'])
-    payload['channel'] = channel
-    payload['reply_channel'] = message.content['reply_channel']
-    Channel("chat.receive").send(payload)
-
-
-@channel_session_user
-def ws_disconnect(message, channel):
-    try:
-        room = Room.find_by_hash_of(message.user, channel)
-        room.channel.discard(message.reply_channel)
-    except:
-        pass
--- chat/routing.py
+++ chat/routing.py
@@ -1,17 +1,10 @@
-from channels.routing import route
-
-from .consumers import (ws_connect, ws_receive, ws_disconnect)
+from .consumers import Consumer
 from .views import chat_receive
+from channels import route
 
 websocket_routing = [
-    route("websocket.connect", ws_connect,
-          path=r"^/(?P<channel>[a-zA-Z0-9_]+)$"),
-    route("websocket.receive", ws_receive,
-          path=r"^/(?P<channel>[a-zA-Z0-9_]+)$"),
-    route("websocket.disconnect", ws_disconnect,
-          path=r"^/(?P<channel>[a-zA-Z0-9_]+)$"),
+    Consumer.as_route(path=r"^/(?P<channel>[a-zA-Z0-9_]+)$"),
 ]
-
 
 custom_routing = [
     route("chat.receive", chat_receive),
